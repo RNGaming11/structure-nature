ded3467f374890b93df7b720a355e6ed669bc90a
RNGaming10 <iytityity04@gmail.com>
2024-07-04 18:29:17 +0200

    a new update

--- src/main/java/net/rngaming/structure/nature/StructureNature.java
+++ src/main/java/net/rngaming/structure/nature/StructureNature.java
@@ -5,8 +5,6 @@ import net.fabricmc.api.ModInitializer;
 import net.rngaming.structure.nature.blocks.ModBlocks;
 import net.rngaming.structure.nature.item.ModItemGroups;
 import net.rngaming.structure.nature.item.ModItems;
-import net.rngaming.structure.nature.villager.ModVillagers;
-import net.rngaming.structure.nature.world.village.VillageAdditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +19,5 @@ public class StructureNature implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
-
-		ModVillagers.registerVillagers();
 	}
 }
