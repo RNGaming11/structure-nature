2c5663edadb67957200fd3e40c19101939751600
RNGaming10 <iytityity04@gmail.com>
2024-07-04 18:28:36 +0200

    a new update

--- src/main/java/net/rngaming/structure/nature/villager/ModVillagers.java
+++ src/main/java/net/rngaming/structure/nature/villager/ModVillagers.java
@@ -1,17 +1,29 @@
 package net.rngaming.structure.nature.villager;
 
+import com.google.common.collect.ImmutableSet;
+import net.fabricmc.fabric.api.object.builder.v1.villager.VillagerProfessionBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
 import net.minecraft.block.Block;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
+import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.poi.PointOfInterestType;
 import net.rngaming.structure.nature.StructureNature;
 
 public class ModVillagers {
 
 
+    public static VillagerProfession registerProfession(String name, RegistryKey<PointOfInterestType> type) {
+        return Registry.register(Registry., Identifier.of(StructureNature.MOD_ID, name),
+                VillagerProfessionBuilder.create().id(Identifier.of(StructureNature.MOD_ID, name)).workstation(type)
+                        .workSound(SoundEvents.ENTITY_VILLAGER_WORK_ARMORER).build());
+    }
+
     public static PointOfInterestType registerPOI(String name, Block block) {
         return PointOfInterestHelper.register(Identifier.of(StructureNature.MOD_ID, name),
-                1, 1, Immuta)
+                1, 1, ImmutableSet.copyOf(block.getStateManager().getStates()));
     }
 
     public static void registerVillagers() {
